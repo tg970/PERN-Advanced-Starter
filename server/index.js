61d95cee391cc337ab10df033ce6c77537bfe902
tg970 <tgoodman@outlook.com>
2018-07-20 12:50:21 -0600

    initial file setup and config

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// const db = require('../db');
+
+const app = express();
+
+const PORT = process.env.PORT || 1122;
+
+app.use(morgan('tiny'));
+app.use(helmet());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(express.static('dist'));
+
+// const users = require('./routers/users');
+
+// app.use('/users', users);
+
+app.get('/api/hello', (req, res) => {
+  res.send({ message: "Hello World!!" });
+});
+
+app.listen(PORT, () => console.log('Server OK'));
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const outputDirectory = "dist";
+
+module.exports = {
+  entry: "./src/client/index.js",
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg|jpg)$/,
+        loader: "url-loader?limit=100000"
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, './src/client'), 'node_modules'],
+    extensions: ['.js', '.jsx', '.json']
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    proxy: {
+      "/api": "http://localhost:1122"
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin([outputDirectory]),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      favicon: "./public/favicon.ico"
+    })
+  ]
+};
