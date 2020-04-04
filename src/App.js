bfa7966cc988636aea7517fbe8d9c3f1da9eea3a
Tyler Goodman <tgoodman@outlook.com>
2020-04-03 21:57:28 -0500

    new home page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import App from './App';
-import LoginPage from './views/LoginPage/LoginPage';
-import Components from './views/Components/Components';
-import LandingPage from './views/LandingPage/LandingPage';
-import ProfilePage from './views/ProfilePage/ProfilePage';
+import HomePage from './views/HomePage/HomePage.js';
+import LoginPage from './views/LoginPage/LoginPage.js';
+import Components from './views/Components/Components.js';
+import LandingPage from './views/LandingPage/LandingPage.js';
+import ProfilePage from './views/ProfilePage/ProfilePage.js';
 
 import "assets/scss/material-kit-react.scss";
 
@@ -20,7 +20,7 @@ const App = ({ store }) => (
   <Provider store={store}>
     <Router>
       <Switch>
-        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/" component={HomePage} />
         <Route path="/login" component={LoginPage} />
         <Route path="/components" component={Components} />
         <Route path="/landing" component={LandingPage} />
---
+++ src/views/HomePage/HomePage.js
@@ -0,0 +1,80 @@
+import React from "react";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+import Paint from "@material-ui/icons/FormatPaint";
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPage.js";
+
+// Sections for this page
+import ProductSection from "./Sections/ProductSection.js";
+import TeamSection from "./Sections/TeamSection.js";
+import WorkSection from "./Sections/WorkSection.js";
+
+import homeBg from "assets/img/bg7.jpg";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+export default function HomePage(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
+  return (
+    <div>
+      <Header
+        color="transparent"
+        routes={dashboardRoutes}
+        brand="PERN Starter"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white"
+        }}
+      />
+      <Parallax filter image={homeBg}>
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={8}>
+              <h1 className={classes.title}>Every App Starts With a Foundation</h1>
+              <h4>
+                This app is designed as a launching point for future production
+                applications. Using the latest and greatest of web development
+                technologies including a full suite UI kit this platform
+                provides a powerful spring board for rapid prototyping and
+                development.
+              </h4>
+              <br />
+              <Button
+                color="info"
+              >
+                <Paint />
+                Find out more
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <ProductSection />
+          <TeamSection />
+          <WorkSection />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
