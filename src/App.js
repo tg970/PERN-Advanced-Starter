c524d1e8ec45cf209b83eaf35e962d3dc86437a9
Tyler Goodman <tgoodman@outlook.com>
2020-04-03 21:33:33 -0500

    reorg app.js and root components

--- src/App.js
+++ src/App.js
@@ -1,64 +1,37 @@
-/* eslint-disable */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import App from './App';
+import LoginPage from './views/LoginPage/LoginPage';
+import Components from './views/Components/Components';
+import LandingPage from './views/LandingPage/LandingPage';
+import ProfilePage from './views/ProfilePage/ProfilePage';
 
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import "assets/scss/material-kit-react.scss";
 
-import Users from "./components/Users/Users";
-import PageLinks from "./components/PageLinks/PageLinks";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/HomeFooter";
+// const indexRoutes = [
+//   { path: "/login", name: "LoginPage", component: LoginPage },
+//   { path: "/about", name: "AboutPage", component: About },
+//   { path: "/", name: "Components", component: App }
+// ];
 
-import "./app.css";
-import ReactImage from "./react.png";
+const App = ({ store }) => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/components" component={Components} />
+        <Route path="/landing" component={LandingPage} />
+        <Route path="/profile" component={ProfilePage} />
+      </Switch>
+    </Router>
+  </Provider>
+);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { greeting: null };
-  }
+App.propTypes = {
+  store: PropTypes.object.isRequired
+};
 
-  componentDidMount() {
-    fetch("/api/hello")
-      .then(res => res.json())
-      .then((data) => {
-        this.setState({ greeting: data.message });
-      });
-  }
-
-  render() {
-    const { classes, ...rest } = this.props;
-    return (
-      <div className="appBody">
-        <Header
-          brand="PERN Starter App"
-          fixed
-          changeColorOnScroll={{
-            height: 400,
-            color: "white"
-          }}
-          home
-          {...rest}
-        />
-        <div className="greeting">
-          <img className="logo" src={ReactImage} alt="react" />
-          {this.state.greeting ? (
-            <h1>
-              {this.state.greeting}
-            </h1>
-          ) : (
-            <h1>
-              Loading, please wait...
-            </h1>
-          )}
-        </div>
-        <div className="sectionWrap">
-          <Users />
-          <PageLinks />
-        </div>
-        <Footer />
-      </div>
-    );
-  }
-}
-
-export default App
+export default App;
